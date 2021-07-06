85dfdd97848589aec256585f9d2136d4eec11c2d
pratyushvatsalshukla <pratyushshukla9333@gmail.com>
2021-07-06 15:10:28 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ client/src/OrderedList.jsx
@@ -0,0 +1,19 @@
+import React from 'react' ;
+function OrderedListing(){
+
+    return(
+    <ol>
+        <li>
+            Pratyush
+        </li>
+        <li>
+            Vatsal
+        </li>
+        <li>
+            Shukla
+        </li>
+    </ol>
+    ) ;
+}
+
+export default OrderedListing ;
---
+++ server/router.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send({ response: "Server Is Active and Running" }).status(200);
+});
+
+module.exports = router;
